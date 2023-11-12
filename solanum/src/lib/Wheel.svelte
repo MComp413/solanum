fc3e0e830ac6f93d7b1a4bf094321bf0d8f05406
unknown <matheus.andrade@inspira.com.br>
2023-11-12 14:51:02 -0300

    implementa roda inicial e seções

--- solanum/src/lib/Wheel.svelte
+++ solanum/src/lib/Wheel.svelte
@@ -1,14 +1,65 @@
 <script lang="ts">
+  import WheelSection from "./WheelSection.svelte";
+
   export let wheelSections: SectionData[];
+  let orderedWheelSections: SectionData[];
+  let totalSections: number;
+
+  $: orderedWheelSections = wheelSections.sort((a, b) => a.order - b.order);
+  $: totalSections = wheelSections.length;
 </script>
 
-<div>
-  <div class="center">🥔</div>
-  <div class="wheelSections">
-    {#each wheelSections as section}
-      <div class="section">
-        {section.label}
-      </div>
-    {/each}
-  </div>
+<div class="wheel">
+  <button class="center"><span>🥔</span></button>
+  {#each orderedWheelSections as section}
+    <WheelSection wheelSection={section} {totalSections} />
+  {/each}
 </div>
+
+<style lang="scss">
+  .wheel {
+    display: flex;
+    position: relative;
+    height: 30rem;
+    width: 30rem;
+
+    overflow: hidden;
+
+    border-radius: 50%;
+    border: 5px solid white;
+  }
+
+  .center::before {
+    content: "";
+    height: 75%;
+    width: 50%;
+    background-color: white;
+    position: absolute;
+    bottom: 90%;
+    clip-path: polygon(50% 0%, 15% 100%, 85% 100%);
+  }
+
+  .center {
+    font-size: 25pt;
+    line-height: 25pt;
+
+    height: 10%;
+    width: 10%;
+    padding: 0;
+    margin: 0;
+    border: 0;
+
+    box-shadow: none;
+    background-color: white;
+
+    position: absolute;
+    align-self: center;
+    left: 45%;
+    z-index: 10;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+  }
+</style>
---
+++ solanum/src/lib/WheelSection.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  export let wheelSection: SectionData;
+  export let totalSections: number;
+
+  $: rotation = (360 / totalSections) * wheelSection.order;
+  $: textRotation = 180 / totalSections;
+  $: fr = 100 / totalSections;
+</script>
+
+<div
+  class="wheel-section"
+  style="transform: rotate({rotation}deg); background-color: {wheelSection.backgroundColor}; "
+>
+  <span style="transform: rotate({textRotation}deg)">{wheelSection.label}</span>
+</div>
+
+<style lang="scss">
+  .wheel-section {
+    position: absolute;
+    transform-origin: bottom right;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 50%;
+    width: 50%;
+    border-radius: 15%;
+
+    span {
+      font-size: xx-large;
+      font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+      font-weight: 700;
+      letter-spacing: 0.15rem;
+      text-shadow: 0px 0px 5px 5px black;
+    }
+  }
+</style>
